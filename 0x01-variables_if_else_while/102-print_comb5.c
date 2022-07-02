5b57c3dd5348d253cdfcf06ef871c7786fb526f9
Amadi Emmanuel Chibuzor <emmanuel.amadi68@gmail.com>
2022-07-01 21:57:59 -0700

    Print Code5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,8 +18,7 @@ for (num2 = num1 + 1; num2 <= 99; num2++)
 putchar((num1 / 10) + '0');
 					
 putchar((num1 % 10) + '0');
-					
-putchar(',');
+				
 					
 putchar((num2 / 10) + '0');
 					
@@ -28,8 +27,6 @@ putchar((num2 % 10) + '0');
 if (num1 == 98 && num2 == 99)
 					
 continue;
-					
-putchar(',');
 					
 putchar(',');
 					
