af001ac47f69746be79dffbcb95a73ae53500687
Amadi Emmanuel Chibuzor <emmanuel.amadi68@gmail.com>
2022-07-02 15:44:43 -0700

    Print Code4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,7 @@ int com = 44;
 while (n1 <= 57)
 {
 n2 = n1 + 1;
-		
+
 while (n2 <= 57)
 {
 n3 = n2 + 1;
@@ -28,16 +28,16 @@ while (n3 <= 57)
 putchar(n1);
 putchar(n2);
 putchar(n3);
-if (n1 != 55 || n2 != 56 || n3 != 57)	
+if (n1 != 55 || n2 != 56 || n3 != 57)
 {
 putchar(com);
 putchar(32);
-}	
-n3 += 1;	
+}
+n3 += 1;
 }
 n2 += 1;
 }
-n1 += 1;	
+n1 += 1;
 }
 putchar('\n');
 return (0);
