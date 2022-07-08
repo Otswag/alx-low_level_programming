0a29adda887a68d2096ee850243db279b4742239
Amadi Emmanuel Chibuzor <emmanuel.amadi68@gmail.com>
2022-07-08 13:51:32 -0700

    Main yet to work

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,9 +8,7 @@ include <stdio.h>
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
-
 i = 0;
 
 /**
@@ -20,7 +18,5 @@ i = 0;
  * }
  */
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
-}
 }
