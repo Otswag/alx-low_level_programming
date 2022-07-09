f29eddbd7588952a4978989b984b16a3f3f701c5
Otswag <106761383+Otswag@users.noreply.github.com>
2022-07-09 22:27:06 +0100

    Largest Number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,60 +1,33 @@
-<<<<<<< HEAD
-#include "main.h" 
-  
- /** 
-  * largest_number - returns the largest of 3 numbers 
-  * @a: first integer 
-  * @b: second integer 
-  * @c: third integer 
-  * Return: largest number 
-  */
-  
- int largest_number(int a, int b, int c) { 
-int largest;
+#include "main.h"
 
-if (a >= b && a >= c)
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
 {
-largest = a;
+int largest;
+
+if (a > b && a > c)
+{
+largest = a;
 }
-else if (b >= a && b >= c)
+else if (a > b && c > a)
 {
-largest = b;
+largest = c;
+}
+else if (b > c)
+{
+largest = b;
 }
 else
 {
-largest = c;
+largest = c;
 }
 
-return (largest);
+return (largest);
 }
-=======
-#include "main.h"
-  
- /** 
-  * largest_number - returns the largest of 3 numbers 
-  * @a: first integer 
-  * @b: second integer 
-  * @c: third integer 
-  * Return: largest number 
-  */ 
-  
- int largest_number(int a, int b, int c) 
- { 
-         int largest; 
-  
-         if (a >= b && a >= c) 
-         { 
-                 largest = a; 
-         } 
-         else if (b >= a && b >= c) 
-         { 
-                 largest = b; 
-         } 
-         else 
-         { 
-                 largest = c; 
-         } 
-  
-         return (largest); 
- }
->>>>>>> 3c951405129785a4d09e74e31ef67b1568b4e20f
