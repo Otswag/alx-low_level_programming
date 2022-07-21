40176671113429a799b5ada7fddaaf23b3ca75ac
Amadi Emmanuel Chibuzor <emmanuel.amadi68@gmail.com>
2022-07-21 04:39:51 -0700

    Puts Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,14 @@
 #include "main.h"
 
 /**
-
  * _puts_recursion -  function that prints a string
-
  *
-
  * @s: string to receive
-
  *
-
  * Return: nothing
-
  */
- 
- void _puts_recursion(char *s)
 
+void _puts_recursion(char *s)
 {
 
 	if (*s == '\0')
