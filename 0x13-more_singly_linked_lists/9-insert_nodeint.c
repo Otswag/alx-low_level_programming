db54504325c3afcaebf95d79c23b32c5c8049722
Otswag <106761383+Otswag@users.noreply.github.com>
2022-08-15 19:42:20 +0100

    Updated insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,53 @@
 #include "lists.h"
+
 /**
- * insert_nodeint_at_index - inserts node at index
- * @head: linked list
- * @idx: index of node
- * @n: value of head
- * Return: newly added node
+ * insert_nodeint_at_index - insert a new node at a given position
+ * @head: pointer to head pointer of linked list
+ * @idx: index to insert new node
+ * @n: new node's data
+ * Return: address of new node, or NULL if failed
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0;
-	listint_t *tmp, *new;
 
+	unsigned int i = 0;
+	listint_t *new_node, *tmp;
+
+	/* account for empty pointer */
 	if (head == NULL)
 		return (NULL);
 
+	/* malloc and set values for new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	/* account for idx 0 */
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* iterate to 1 before nth index to insert */
 	tmp = *head;
-	while (tmp != NULL && count != idx - 1)
+	while (i < (idx - 1))
 	{
 		tmp = tmp->next;
-		count++;
+		i++;
+
+		if (tmp == NULL) /* account for idx out of range */
+		{
+			free(new_node);
+			return (NULL);
+		}
 	}
 
-	if (count != idx - 1 && idx != 0)
-		return (NULL);
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	if (idx != 0)
-	{
-		new->next = tmp->next;
-		tmp->next = new;
-	}
-	else
-	{
-		new->next = *head;
-		*head = new;
-	}
-	return (new);
+	/* link into list */
+	new_node->next = tmp->next;
+	tmp->next = new_node;
+	return (new_node);
 }
