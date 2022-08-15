e8dd2e279e23f46cb92a28babbc02e6ce0dc80c8
Otswag <106761383+Otswag@users.noreply.github.com>
2022-08-15 19:01:16 +0100

    Updated insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -59,7 +59,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
  * Return: the address of new node
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
 	listint_t *tmp;
