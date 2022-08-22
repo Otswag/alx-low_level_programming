5314765c0b69fd3fca757d4ebca8945356ded457
Otswag <106761383+Otswag@users.noreply.github.com>
2022-08-22 21:53:42 +0100

    Updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,4 +4,11 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void printabi(char *head)
+void printversion(char *head)
+void printdata(char *head)
+void printclass(char *head)
+void printtype(char *head)
+int main(int ac, char *av[])
+void printentry(char *head)
 #endif /* #ifndef _MAIN_H */
