36490fc85b98281cfb036be7e940be8e7779eb52
Otswag <106761383+Otswag@users.noreply.github.com>
2022-08-22 22:05:25 +0100

    Updated main header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,26 @@
 
 #ifndef _MAIN_H
 #define _MAIN_H
+
+include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void printabi(char *head)
-void printversion(char *head)
-void printdata(char *head)
-void printclass(char *head)
-void printtype(char *head)
-int main(int ac, char *av[])
-void printentry(char *head)
+int _putchar(char c);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void validate_elf(unsigned char *e_ident);
+unsigned int lit_to_big_endian(unsigned int x);
+void print_entry(unsigned int e_type, unsigned char *e_ident);
 #endif /* #ifndef _MAIN_H */
