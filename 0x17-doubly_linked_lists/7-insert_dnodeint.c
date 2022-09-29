dbf4574726dafd6d9cb0adaa84d9e6d9bdaf7ab5
Otswag <106761383+Otswag@users.noreply.github.com>
2022-09-29 21:04:21 +0100

    Updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,52 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - insert node at index?
- * @h: address of linked list
- * @idx: index to place list
- * @n: value of n
- * Return: return new node or NULL
+ * insert_dnodeint_at_index - inserts a new node at given position
+ * @h: head of the node
+ * @idx: index where to place node
+ * @n: data for the node
+ * Return: address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp, *fresh;
-	unsigned int dex = 1;
+	dlistint_t *tmp = *h, *tmpcount = *h, *tmp2;
+	dlistint_t *new_node;
+	unsigned int cont = 0;
 
-	tmp = *h;
-	fresh = malloc(sizeof(dlistint_t));
-	if (fresh == NULL)
+	if (*h == NULL)
 		return (NULL);
-	fresh->n = n;
-	fresh->prev = NULL;
-	fresh->next = NULL;
-	if ((*h) == NULL)
-	{
-		if (idx == 0)
-		{
-			*h = fresh;
-			return (fresh);
-		}
+	while (tmpcount != NULL)
+		tmpcount = tmpcount->next, cont++;
+	if (idx > cont)
 		return (NULL);
-	}
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
 	if (idx == 0)
 	{
-		fresh->next = *h;
-		(*h)->prev = fresh;
-		*h = fresh;
-		return (fresh);
+		new_node->prev = NULL, new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
 	}
-	while (tmp->next != NULL && dex != idx)
-	{
-		tmp = tmp->next;
-		dex++;
-	}
-	if (dex == idx)
-	{
-		fresh->prev = tmp;
-		fresh->next = tmp->next;
-		if (tmp->next != NULL)
-			tmp->next->prev = fresh;
-		tmp->next = fresh;
-		return (fresh);
-	}
-	return (NULL);
+	for (cont = 0; cont < idx; cont++)
+		tmp2 = tmp, tmp = tmp->next;
+	new_node->next = tmp;
+	new_node->prev = tmp2;
+	tmp2->next = new_node;
+	if (tmp)
+		tmp->prev = new_node;
+	return (new_node);
 }
